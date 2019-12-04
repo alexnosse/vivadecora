e78938f65f93253187a042f4c37cb9f1e34fb03f
Alex Nosse <a.k.nosse@gmail.com>
2019-12-03 23:49:42 -0400

    WIP

--- src/reducers/movies.js
+++ src/reducers/movies.js
@@ -2,12 +2,17 @@ import {
   GET_MOVIES_REQUEST,
   GET_MOVIES_SUCCESS,
   GET_MOVIES_FAILURE,
+  GET_MOVIE_REQUEST,
+  GET_MOVIE_SUCCESS,
+  GET_MOVIE_FAILURE,
 } from "../actions/movies";
 
 const initialState = {
   collection: {},
   isFetched: false,
   url: "https://api.themoviedb.org/4/list/126912",
+  currentMovie: null,
+  isMovieFetched: false,
 };
 
 export default function(state = initialState, action) {
@@ -28,6 +33,24 @@ export default function(state = initialState, action) {
       };
 
     case GET_MOVIES_FAILURE:
+      return {
+        ...state,
+        isFetched: false,
+      };
+    case GET_MOVIE_REQUEST:
+      return {
+        ...state,
+        isFetched: true,
+      };
+
+    case GET_MOVIE_SUCCESS:
+      return {
+        ...state,
+        // TODO: update the current movie
+        isFetched: false,
+      };
+
+    case GET_MOVIE_FAILURE:
       return {
         ...state,
         isFetched: false,
--- src/ui/pages/movies/movies.page.js
+++ src/ui/pages/movies/movies.page.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { getMovies, getMovie } from "../../../actions/movies";
 
-const MoviesPage = () => {
+const mapStateToProps = state => {
+  const { collection, currentMovie } = state.movies;
+  return {
+    collection,
+    currentMovie,
+  };
+};
+
+const mapDispatchToProps = {
+  getMovies,
+  getMovie,
+};
+
+const MoviesPage = props => {
+  const { getMovies, currentMovie } = props;
+
+  useEffect(() => {
+    getMovies("https://api.themoviedb.org/4/list/126912");
+  }, []);
+
   return <div className="movies"> Movies Page =3</div>;
 };
 
-export default MoviesPage;
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(MoviesPage);
